3594f5dcb455f8490b63796ae2b4c271b3a6b2c8
lerotsy <rotsyrazafy@gmail.com>
2024-01-05 20:15:54 -0500

    add support for SET and GET

--- app/redis_server.py
+++ app/redis_server.py
@@ -1,6 +1,9 @@
 import socket
+import threading
 
 EOR = "\r\n"  # End of Response
+
+
 class RedisServer:
     def __init__(self, host='127.0.0.1', port=6379):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,9 +13,11 @@ class RedisServer:
         self.data_store = {}
 
     def listen(self):
-        client_socket, addr = self.server_socket.accept()
-        print(f"Connection from {addr}")
-        self.handle_client(client_socket)
+        while True:
+            client_socket, addr = self.server_socket.accept()
+            print(f"Connection from {addr}")
+            threading.Thread(target=self.handle_client,
+                             args=[client_socket]).start()
 
     def handle_client(self, client_socket):
         try:
@@ -34,7 +39,12 @@ class RedisServer:
         if command == 'PING':
             response = '+PONG'
         elif command == 'ECHO':
-            response = ''
+            response = " ".join(parts[1:])
+        elif command == 'SET':
+            self.data_store[parts[1]] = parts[2]
+            return "+OK"
+        elif command == 'GET':
+            response = self.data_store.get(parts[1], '(nil)')
         else:
             response = '-ERR Unknown Commnad'
         return f"{response}{EOR}"
